e229189fc4aa759c01c0ee3f54fdbe3cf2ea5917
Stanislav Seletskiy <s.seletskiy@gmail.com>
2015-08-03 20:42:15 +0600

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+Supplementary program for
+[ash-mailcap](https://github.com/seletskiy/ash-mailcap).
+
+# Installation
+
+## ArchLinux
+
+Via (AUR)[https://aur4.archlinux.org/packages/ash-mailcap-autoview] or self
+built package:
+```
+git clone https://github.com/seletskiy/ash-mailcap-autoview
+git checkout pkgbuld
+makepkg
+```
+
+## go get
+
+```
+go get github.com/seletskiy/ash-mailcap-autoview
+```
+
+# Usage
+
+See [ash-mailcap#autoview](https://github.com/seletskiy/ash-mailcap#autoview)
+for usage.
---
+++ main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"regexp"
+	"strings"
+
+	"github.com/docopt/docopt-go"
+)
+
+const usage = `Supplementary program for ash-mailcap.
+
+Reads ash-formatted overview diff and outputs section which contains
+specified comment.
+
+Exits with 1 if comment not found.
+Exits with 2 if there are problems reading file.
+
+Usage:
+  $0 -h | --help
+  $0 <file> <comment-id>
+`
+
+var reComment = regexp.MustCompile(`^#\s+\[(\d+)@\d+\]`)
+
+func main() {
+	args, err := docopt.Parse(usage, nil, true, "1.0", false)
+	if err != nil {
+		panic(err)
+	}
+
+	file, err := os.Open(args["<file>"].(string))
+	if err != nil {
+		log.Printf("can't open specified file: %s", err)
+	}
+
+	commentFound := false
+
+	section := []string{}
+
+	reader := bufio.NewReader(file)
+	for {
+		line, err := reader.ReadString('\n')
+		//log.Printf("main.go:42 %#v", line)
+		if err != nil {
+			if err == io.EOF {
+				os.Exit(1)
+			}
+
+			log.Printf("error while reading file: %s", err)
+			os.Exit(2)
+		}
+
+		if line == "\n" {
+			if commentFound {
+				fmt.Print(strings.Join(section, ""))
+				os.Exit(0)
+			}
+
+			section = make([]string, 0)
+		} else {
+			section = append(section, line)
+		}
+
+		matches := reComment.FindStringSubmatch(line)
+		if len(matches) > 0 && matches[1] == args["<comment-id>"].(string) {
+			commentFound = true
+		}
+	}
+}
